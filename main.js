6c4c5dc0b0e07eeea3cd5a0bba98aea0c93c223d
gamedevandcoder <113296779+gamedevandcoder@users.noreply.github.com>
2022-10-08 14:11:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
     bool = true
     swp = false
+    bl2 = false
+    it = setInterval(changeimg, 1500)
     function show(){
-        portfoilo = document.getElementById("portfoilo")
-        portfoilo.style.opacity = 1
+        bl2=true
+        
+    }
+    function cur(){
+        if (swp==false){
+        document.getElementById("u1").style.cursor = "default"
+        }
+        else {
+            document.getElementById("u1").style.cursor = "pointer"
+        }
+    }
+    function changeimg() {
+        if (num %2 == 1) {
+            document.getElementById("img01").src = "02.png";
+        }
+        else {
+            document.getElementById("img01").src = "01.png";
+        }
     }
     function stt(){
         if (bool == true){
             start()
         }
+        
+    }
+    function dbvd() {
+        if (swp==true){
+        window.open("https://github.com/gamedevandcoder/db-video-downloader", '_blank').focus();
+      }
     }
     num = 0
     function start() {
             bool = false
+            
             speed = 100
             count = 0
-
             let string ='Sudo -u Show My Portfoilo        <br>'+'>'
             newstr = ""
             under ="&nbsp"
@@ -27,6 +51,7 @@
                 under ="_"
                 if (num %2 == 1) {
                     under="&nbsp"
+                    
                 }
                 else {
                     under = "_"
@@ -40,6 +65,7 @@
                 }
                 if (swp == true){
                     show()
+                    document.getElementById("hide").style.opacity = 1
                 }
             }
         }
